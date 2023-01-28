0fef89816535f397635b1d7bb3271471a5f912b5
Anatoly Rubanov <anatolijrubanov@gmail.com>
2023-01-28 21:46:43 +0300

    function-style refactoring

--- src/extraction/indexer.py
+++ src/extraction/indexer.py
@@ -18,76 +18,69 @@ s3 = boto3.client(
     )
 bucket = config['bucket_name']
 
+base_url = "https://archive.sensor.community/"
+link_pattern = re.compile('\d\d\d\d-\d\d-\d\d/')
+index_folder = 'file_index/new/'
+data_dir = '/tmp/data/'
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
 
-class FileIndexer:
+def build_index():
+    """Collect file index and upload it on S3. Using multithreading"""
+    resp = httpx.get(base_url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    links = [link.get('href') for link in soup.find_all('a')]
+    pool = ThreadPool(processes=10)
+    for result in pool.map(check_and_scan_deeper, links):
+        if result:
+            print(f"{result} downloaded")
 
-    def __init__(self) -> None:
-        self.base_url = "https://archive.sensor.community/"
-        self.link_pattern = re.compile('\d\d\d\d-\d\d-\d\d/')
-        self.index_folder = 'file_index/new/'
-        self.data_dir = 'data/'
-        if not os.path.exists(self.data_dir):
-            os.mkdir(self.data_dir)
+def check_and_scan_deeper(link: str) -> Optional[str]:
+    """
+    download list of links for single date, save it into file and upload to s3
+    """
+    if not check_link(link):
+        print(f"skip {link}")
+        return
 
-    def build_index(self):
-        """Collect file index and upload it on S3. Using multithreading"""
-        resp = httpx.get(self.base_url)
-        soup = BeautifulSoup(resp.text, 'lxml')
-        links = [link.get('href') for link in soup.find_all('a')]
-        pool = ThreadPool(processes=10)
-        for result in map(self._check_and_scan_deeper, links):
-            if result:
-                print(f"{result} downloaded")
-        print(len(self.index))
+    print(f"downloading {link}")
+    url = base_url + link
+    resp = httpx.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    links = [item.get('href') for item in soup.find_all('a')]
+    files = [url+item for item in links if '.csv' in item]
+    data_file = data_dir + link[:-1] + '.txt'
+    data_object = index_folder + link[:-1] + '.txt'
+    with open(data_file, "w") as f:
+        for line in files:
+            f.write(line+'\n')
 
-    def _check_and_scan_deeper(self, link: str) -> Optional[str]:
-        """
-        download list of links for single date, save it into file and
-        upload to s3
-        """
-        if not self._check_link(link):
-            print(f"skip {link}")
-            return
-
-        print(f"downloading {link}")
-        url = self.base_url + link
-        resp = httpx.get(url)
-        soup = BeautifulSoup(resp.text, 'lxml')
-        links = [item.get('href') for item in soup.find_all('a')]
-        files = [url+item for item in links if '.csv' in item]
-        data_file = self.data_dir + link[:-1] + '.txt'
-        data_object = self.index_folder + link[:-1] + '.txt'
-        with open(data_file, "w") as f:
-            for line in files:
-                f.write(line+'\n')
-
-        try:
-            s3.upload_file(data_file, bucket, data_object)
-            return data_object        
-        except botocore.exceptions.ClientError as e:
-            print(e)
-        finally:
-            os.remove(data_file)
+    try:
+        s3.upload_file(data_file, bucket, data_object)
+        return data_object        
+    except botocore.exceptions.ClientError as e:
+        print(e)
+    finally:
+        os.remove(data_file)
               
 
-    def _check_link(self, link: str) -> bool:
-        "check link format and if file already exist on s3"
-        if not self.link_pattern.match(link):
-            return False
-        file = self.index_folder + link[:-1] + '.txt'
-        try:
-            s3.head_object(Bucket=bucket, Key=file)
-            return False # file already exist
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == "404":
-                # The object does not exist.
-                pass
-            else:
-                # Something else has gone wrong.
-                raise
-        return True
+def check_link(link: str) -> bool:
+    "check link format and if file already exist on s3"
+    if not link_pattern.match(link):
+        return False
+    file = index_folder + link[:-1] + '.txt'
+    try:
+        s3.head_object(Bucket=bucket, Key=file)
+        return False # file already exist
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # The object does not exist.
+            pass
+        else:
+            # Something else has gone wrong.
+            raise
+    return True
 
 
 if __name__ == "__main__":
-    indexer = FileIndexer()
-    indexer.build_index()
+    build_index()
--- src/extraction/loader.py
+++ src/extraction/loader.py
@@ -15,32 +15,28 @@ s3 = boto3.client(
     aws_secret_access_key=config["aws_secret_access_key"],
 )
 bucket = config["bucket_name"]
+index_folder = "file_index/new/"
+processed_folder = "file_index/processed/"
+files_folder = "files/new/"
+data_dir = "/tmp/data/"
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
 
-
-class FileLoader:
-    def __init__(self) -> None:
-        self.index_folder = "file_index/new/"
-        self.processed_folder = "file_index/processed/"
-        self.files_folder = "files/new/"
-        self.data_dir = "data/"
-        if not os.path.exists(self.data_dir):
-            os.mkdir(self.data_dir)
-
-    def load_file(self, link: str):
-        """
-        link like: https://archive.sensor.community/2015-10-01/2015-10-01_ppd42ns_sensor_27.csv
-        """
-        resp = httpx.get(link)
-        data = resp.content.decode()
-        path = self.data_dir + link.split("/").join()
-        data_object = self.files_folder + link.split("/").join()
-        print(path)
-        with open(path, 'w') as f:
-            f.write(data)
-        try:
-            s3.upload_file(path, bucket, data_object)
-            return data_object        
-        except botocore.exceptions.ClientError as e:
-            print(e)
-        finally:
-            os.remove(path)
+def load_file(link: str):
+    """
+    link like: https://archive.sensor.community/2015-10-01/2015-10-01_ppd42ns_sensor_27.csv
+    """
+    resp = httpx.get(link)
+    data = resp.content.decode()
+    path = data_dir + link.split("/").join()
+    data_object = files_folder + link.split("/").join()
+    print(path)
+    with open(path, 'w') as f:
+        f.write(data)
+    try:
+        s3.upload_file(path, bucket, data_object)
+        return data_object        
+    except botocore.exceptions.ClientError as e:
+        print(e)
+    finally:
+        os.remove(path)
