46f9efd44b3cfb23ff5840694fac2e6a8a92be2e
Anatoly Rubanov <anatolijrubanov@gmail.com>
2023-03-31 19:04:07 +0300

    add uploading mesurements to s3 bucket

--- pipeline/transformation/process_data/src/app.py
+++ pipeline/transformation/process_data/src/app.py
@@ -3,8 +3,9 @@ import boto3
 import pandas as pd
 
 
-bucket = boto3.resource('s3').Bucket("staging-area-bucket")
-data_dir = '/tmp/data'
+source_bucket = boto3.resource('s3').Bucket("staging-area-bucket")
+target_bucket = boto3.resource('s3').Bucket("transformed-bucket")
+data_dir = 'tmp/data'
 prefix = 'files/new/'
 data_colums = {'timestamp', 'temperature', 'humidity', 'pressure', 'P1', 'P2', 'sensor_id'}
 if not os.path.exists(data_dir):
@@ -17,26 +18,40 @@ def handler(event, context):
     filename = event['file']
     data = get_s3_file_data(filename)
     data = extract_data(data)
-    return data
+    result = write_data_to_s3(data)
+    print(f"File: {result} uploaded to s3")
 
 
 def get_s3_file_data(filename: str) -> pd.DataFrame:
-    bucket.download_file(prefix+filename, os.path.join(data_dir, filename))
+    source_bucket.download_file(prefix+filename, os.path.join(data_dir, filename))
     df = pd.read_csv(os.path.join(data_dir, filename), parse_dates=['timestamp'], sep=';')
     os.remove(os.path.join(data_dir, filename))
     return df
 
 
-def extract_data(df: pd.DataFrame) -> dict:
+def extract_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df[[col for col in df.columns if col in data_colums]]
     df = df.set_index('timestamp')
     df = df.resample('1H').mean()
-    data = {col: df[col].tolist() for col in df.columns}
-    data['timestamp'] = df.index.format()
-    return data
+    df['sensor_id'] = df['sensor_id'].astype(int)
+    # data = {col: df[col].tolist() for col in df.columns}
+    # data['timestamp'] = df.index.format()
+    # return data
+    return df
     
+
+def write_data_to_s3(data: pd.DataFrame) -> str:
+    prefix = 'concentration/' if 'P1' in data.columns else 'temperature/'
+    date = data.index[0].date()
+    sensor_id = data['sensor_id'].iloc[0]
+    data.to_csv(os.path.join(data_dir, f'{date}_{sensor_id}.csv'))
+    target_bucket.upload_file(os.path.join(data_dir, f'{date}_{sensor_id}.csv'), prefix+f'{date}_{sensor_id}.csv')
+    os.remove(os.path.join(data_dir, f'{date}_{sensor_id}.csv'))
+    return prefix+f'{date}_{sensor_id}.csv'
+
 
 if __name__ == "__main__":
     data = get_s3_file_data('2017-08-05_bme280_sensor_1207.csv')
     print(data.head())
-    print(extract_data(data))
+    df = extract_data(data)
+    write_data_to_s3(df)
--- pipeline/transformation/process_location/src/app.py
+++ pipeline/transformation/process_location/src/app.py
@@ -7,7 +7,6 @@ bucket = boto3.resource('s3').Bucket("staging-area-bucket")
 geolocator = geopy.geocoders.Nominatim(user_agent='air-data-pipline')
 data_dir = '/tmp/data'
 prefix = 'files/new/'
-data_colums = {'timestamp', 'temperature', 'humidity', 'pressure', 'P1', 'P2', 'sensor_id'}
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 
